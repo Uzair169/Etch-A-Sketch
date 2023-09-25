c42bb8d7219c509c41794bf2dcc7ecb0ded08ef0
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2023-09-26 00:43:53 +0500

    ADD HTML, CSS & JS

---
+++ script.js
@@ -0,0 +1,53 @@
+function gridDisplay(makeDivs) {
+    const container = document.querySelector('.container');
+
+    for (let i = 0; i < makeDivs; i++) {
+        const div = document.createElement('div');
+        div.style.cssText = " height: 16px; width: 16px";
+        div.className = "boxes";
+        container.appendChild(div);
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "#454545";
+        });
+    }
+    container.style.width = "256px";
+    container.style.height = "256px";
+
+
+}
+
+gridDisplay(256);
+
+function createGrid() {
+    const gridSize = parseInt(prompt("How much Grid Size do you want? (Max: 100)"));
+    const container = document.querySelector('.container');
+    container.style.width = "500px";
+    container.style.height = "500px";
+    for (let j = 0; j < gridSize; j++) {
+        for (let i = 0; i < gridSize; i++) {
+            const div = document.createElement('div');
+            const val = parseInt(container.style.width) / gridSize;
+            div.style.width = `${val}px`;
+            div.style.height = `${val}px`;
+            console.log(div.style.width);
+            console.log(parseInt(container.style.width));
+            div.style.height = (container.style.width) / gridSize;
+            div.className = "boxes";
+            container.appendChild(div);
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = "#454545";
+            });
+        }
+    }
+
+}
+const button = document.getElementById('change');
+
+button.addEventListener('click', () => {
+    let divs = document.querySelectorAll('.boxes');
+    divs.forEach(divs => {
+        divs.remove();
+    });
+    createGrid();
+});
+
